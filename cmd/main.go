ba60c26fabeffcd1e202cad955f61ca4bce9b971
dan-acsc <dan-acsc@users.noreply.github.com>
2026-08-31 23:36:45 +0000

    modify usage

--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,7 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "azul-backup",
+	Use:   "backup",
 	Short: "Azul event backup & restore",
 	Long:  `Azul event backup & restore`,
 }
